c5a99b8ab2b02d30c1019d79f2e96df7ef579285
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2024-02-10 23:18:13 -0800

    added light projection
    projects ray at landing

--- Roll A Ball/Assets/Scripts/PlayerController.cs
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;   
+
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
@@ -12,18 +12,19 @@ public class PlayerController : MonoBehaviour
     public float gravity = 20f;
 
     private int count;
-    public int winValue = 14;//allows user to easily switch requirements for victory & UI will still work
+    public int winValue = 14;
 
-
-    public TextMeshProUGUI countText;   
+    public TextMeshProUGUI countText;
     public GameObject winText;
     public List<GameObject> pickUps = new List<GameObject>();
 
-
     public CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
-    #endregion 
-    
+
+    public LineRenderer jumpPreviewLine;
+    public int jumpPreviewResolution = 50;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,63 +33,100 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winText.SetActive(false);
+
+        jumpPreviewLine.positionCount = jumpPreviewResolution;
+        jumpPreviewLine.enabled = false;
     }
- 
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= winValue)
+        if (count >= winValue)
         {
             winText.SetActive(true);
         }
     }
+
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(xInput,0f,yInput);
+
+        Vector3 movement = new Vector3(xInput, 0f, yInput);
         movement = transform.TransformDirection(movement);
-        movement*=moveSpeed;
-        
-        if(controller.isGrounded)
+        movement *= moveSpeed;
+
+        if (controller.isGrounded)
         {
             moveDirection = movement;
-            if(Input.GetButton("Jump"))
+            if (Input.GetButton("Jump"))
             {
                 moveDirection.y = jumpForce;
             }
         }
         else
         {
-            moveDirection.y -= gravity*Time.deltaTime;
-            
+            moveDirection.y -= gravity * Time.deltaTime;
         }
-        
-        controller. Move(moveDirection*Time.deltaTime);
 
-    if(pickUps.Count > 0)
-    {
-        for(int i = 0; i < pickUps.Count; i++) //A loop that delete's an unused index
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (pickUps.Count > 0)
         {
-            if(pickUps[i] == null)
+            for (int i = 0; i < pickUps.Count; i++)
             {
-                pickUps.RemoveAt(i);
-
-                i--;
+                if (pickUps[i] == null)
+                {
+                    pickUps.RemoveAt(i);
+                    i--;
+                }
             }
         }
+
+        if (Input.GetButton("Jump"))
+        {
+            ShowJumpPreview();
+        }
+        else
+        {
+            HideJumpPreview();
+        }
     }
 
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("PickUp"))
         {
-            Destroy(other.gameObject);//removes game objects
-            count = count + 1;  
+            Destroy(other.gameObject);
+            count = count + 1;
             SetCountText();
         }
-    }       
+    }
+
+ void ShowJumpPreview()
+{
+    jumpPreviewLine.enabled = true;
+
+    float timeInterval = 0.1f; // Adjust this for smoother preview
+
+    // Calculate landing position
+    float totalAirTime = (2f * jumpForce) / gravity;
+    Vector3 landingPosition = transform.position + moveDirection * moveSpeed * totalAirTime;
+
+    // Draw line to landing position
+    jumpPreviewLine.positionCount = 2;
+    jumpPreviewLine.SetPosition(0, transform.position);
+    jumpPreviewLine.SetPosition(1, landingPosition);
+}
+
+
+
+
+
+
+    void HideJumpPreview()
+    {
+        jumpPreviewLine.enabled = false;
+    }
 }
